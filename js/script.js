f1ed9ea2f90634f83b19ee3e37f782816aca713f
Andrea Raguso <andrea.raguso.mailclass@gmail.com>
2022-12-21 20:06:14 +0100

    Esercizio finito con grafica

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 
-for (let i = 0; i <= 100 ; i++){
+for (let i = 0; i <= 104 ; i++){
 
     if( i % 3 == 0 && i % 5 == 0){
         let num = document.getElementById('number');
-        const fizzBuzz= document.createElement("span");
-        fizzBuzz.style.backgroundColor = "red";
+        const fizzBuzz= document.createElement("li");
+        fizzBuzz.style.backgroundColor = "#f0466f";
         fizzBuzz.textContent = "FizzBuzz";
         num.append(fizzBuzz);
     }
 
     else if(i % 3 == 0){
         let num = document.getElementById('number');
-        const fizz= document.createElement("span");
-        fizz.style.backgroundColor = "red";
+        const fizz= document.createElement("li");
+        fizz.style.backgroundColor = "#0cd6a1";
         fizz.textContent = "Fizz";
         num.append(fizz);
     }
     else if(i % 5 == 0){
         let num = document.getElementById('number');
-        const buzz= document.createElement("span");
-        buzz.style.backgroundColor = "red";
+        const buzz= document.createElement("li");
+        buzz.style.backgroundColor = "#ffd166";
         buzz.textContent = "Buzz";
         num.append(buzz);
     }
 
     else{
         let num = document.getElementById('number');
-        const normal= document.createElement("span");
-        normal.style.backgroundColor = "red";
+        const normal= document.createElement("li");
+        normal.style.backgroundColor = "#1389b2";
         normal.textContent = i;
         num.append(normal);
     }
